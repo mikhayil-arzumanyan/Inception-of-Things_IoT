092d8f5071f3b14cc7bccee433b9b70ff4645845
Mikhayil <mikhayil.arzumanyan@gmail.com>
2023-08-27 20:12:06 +0400

    Change INFO to DOING

--- p1/scripts/sworker.sh
+++ p1/scripts/sworker.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-export NAME="emaugaleSW"
-echo "[INFO]  Installing k3s on server worker node (ip: $2)"
+export NAME="miarzumaSW"
+echo "DOING -->  Installing k3s on server worker node (ip: $2)"
 
 export TOKEN_FILE="/vagrant/scripts/node-token"
 
-echo "[INFO]  Token: $(cat $TOKEN_FILE)"
+echo "DOING -->  Token: $(cat $TOKEN_FILE)"
 
 export INSTALL_K3S_EXEC="agent --server https://$1:6443 --token-file $TOKEN_FILE --node-ip=$2"
 
-echo "[INFO]  ARGUMENT PASSED TO INSTALL_K3S_EXEC: $INSTALL_K3S_EXEC"
+echo "DOING -->  ARGUMENT PASSED TO INSTALL_K3S_EXEC: $INSTALL_K3S_EXEC"
 
 apk add curl
 
 curl -sfL https://get.k3s.io | sh -
 
-echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh # way to add alias on all users
+echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh
 
