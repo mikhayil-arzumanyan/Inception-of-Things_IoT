047ad7c611efa95a687c5219f0c38f35f0fc5760
Mikhayil <mikhayil.arzumanyan@gmail.com>
2023-09-02 20:29:23 +0400

    work in p2

---
+++ p2/scripts/server.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "DOING -->  Installing k3s on server node (ip: $1)"
+
+export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1  --bind-address=$1 --advertise-address=$1 "
+
+echo "DOING -->  ARGUMENT PASSED TO INSTALL_K3S_EXEC: $INSTALL_K3S_EXEC"
+
+apk add curl
+
+curl -sfL https://get.k3s.io |  sh -
+
+echo "DOING -->  Doing some sleep to wait for k3s to be ready"
+
+echo "DOING -->  Successfully installed k3s on server node"
+
+echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh
+
+sleep 10
+
+kubectl apply -f /vagrant/scripts/services.yml; sleep 2
+
+kubectl apply -f /vagrant/scripts/app1.yml; sleep 2
+
+kubectl apply -f /vagrant/scripts/app2.yml; sleep 2
+
+kubectl apply -f /vagrant/scripts/app3.yml; sleep 2
+
+kubectl apply -f /vagrant/scripts/ingress.yml; sleep 2
