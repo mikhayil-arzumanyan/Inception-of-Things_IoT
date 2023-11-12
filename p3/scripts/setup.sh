e4dee74ee52b6aaecf0e1d59f4767d9e0dad4b33
mikhayil-arzumanyan <mikhayil.arzumanyan@gmail.com>
2023-11-12 19:52:08 +0400

    work corect after delete all k3d clasters

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -2,7 +2,6 @@
 
 # install docker
 echo "installing docker"
-
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -12,7 +11,6 @@ sudo apt-get install -y docker-ce
 
 #install kubectl
 echo "installing kubectl"
-
 sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
@@ -22,22 +20,23 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 echo "installing k3d"
 sudo curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
-
 #Create k3d clutser with port forwarding
-#Install argocd CLI
-sudo url -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
-sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
-sudo rm argocd-linux-amd64sudo k3d cluster create -p "8080:443@loadbalancer" -p "8888:8888@loadbalancer"
+sudo k3d cluster create -p "8080:443@loadbalancer" -p "8888:8888@loadbalancer"
 
 #Create namespaces:
 sudo kubectl create ns argocd
 sudo kubectl create ns dev
 
 #Setting up Argo CD
-sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stabil/manifests/install.yaml
+sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+sudo sleep 5
+
+#Install argocd CLI
+sudo curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+sudo rm argocd-linux-amd64 
 
 #Make password in argocd
-
 PASSWD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 echo $PASSWD > ./argo_pass.txt
 echo "Username = admin"
