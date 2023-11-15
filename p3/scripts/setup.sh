67e5178f5cdcff8bbf814595fd311ad95b2b3e3c
mikhayil-arzumanyan <mikhayil.arzumanyan@gmail.com>
2023-11-15 21:07:29 +0400

    finish k=kubectl

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -1,40 +1,47 @@
 #! /bin/bash
 
+#Do k instead of kubectl
+echo "#miarzuma" >> ~/.bashrc
+echo "alias sudo='sudo '" >> ~/.bashrc
+echo "alias k='kubectl'" >> ~/.bashrc
+echo "alias k='sudo kubectl'" >> ~/.bashrc
+sudo cp "/root/.bashrc" "/home/$SUDO_USER/.bashrc"
+
 # install docker
 echo "installing docker"
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
-sudo apt-cache policy docker-ce -y
-sudo apt-get install -y docker-ce
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
+apt-cache policy docker-ce -y
+apt-get install -y docker-ce
 
 #install kubectl
 echo "installing kubectl"
-sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
 
 
 # install k3d
 echo "installing k3d"
-sudo curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
 #Create k3d clutser with port forwarding
-sudo k3d cluster create -p "8080:443@loadbalancer" -p "8888:8888@loadbalancer"
+k3d cluster create -p "8080:443@loadbalancer" -p "8888:8888@loadbalancer"
 
 #Create namespaces:
-sudo kubectl create ns argocd
-sudo kubectl create ns dev
+kubectl create ns argocd
+kubectl create ns dev
 
 #Setting up Argo CD
-sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-sudo sleep 5
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+sleep 5
 
 #Install argocd CLI
-sudo curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
-sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
-sudo rm argocd-linux-amd64 
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64 
 
 #Make password in argocd
 PASSWD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
@@ -43,15 +50,15 @@ echo "Username = admin"
 echo "Password = (go to argo_pass.txt file)"
 
 #Create the app in argocd
-sudo kubectl apply -f ../confs/app.yaml -n argocd
+kubectl apply -f ../confs/app.yaml -n argocd
 
 #Create Argo CD App
-sudo argocd app create wil-playground \
+argocd app create wil-playground \
        --repo https://github.com/mikhayil-arzumanyan/IoT-test-for-Inception-of-Things_IoT \
        --path manifests \
        --dest-server https://kubernetes.default.svc \
        --dest-namespace dew222222222222222222222
 
 #Argocd-server for browser
-sudo kubectl port-forward service/argocd-server -n argocd 8080:443
+kubectl port-forward service/argocd-server -n argocd 8080:443
 
